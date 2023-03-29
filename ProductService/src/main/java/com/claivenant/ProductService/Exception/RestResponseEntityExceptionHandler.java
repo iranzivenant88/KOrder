8e54e91bbbf7751648d8553cc271f79ed8f3a29f
iranzivenant88 <88venant@gmail.com>
2023-03-28 21:51:40 -0600

    c3

---
+++ ProductService/src/main/java/com/claivenant/ProductService/Exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.claivenant.ProductService.Exception;
+
+import com.claivenant.ProductService.Model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponseException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(ProductServiceCustomException.class)
+    public ResponseEntity<ErrorResponse>handleProductServiceException(ProductServiceCustomException exception){
+        return new ResponseEntity<>(new ErrorResponse().builder()
+                .errorMessage(exception.getMessage())
+                .errorCode(exception.getErrorCode())
+                .build(),HttpStatus.NOT_FOUND);
+    }
+}
