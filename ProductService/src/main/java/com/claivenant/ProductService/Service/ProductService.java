edc2ae3269628e20f5947a0340d12c1b56e8eb31
iranzivenant88 <88venant@gmail.com>
2023-03-28 21:51:14 -0600

    c3

---
+++ ProductService/src/main/java/com/claivenant/ProductService/Service/ProductService.java
@@ -0,0 +1,13 @@
+package com.claivenant.ProductService.Service;
+
+import com.claivenant.ProductService.Model.ProductRequest;
+import com.claivenant.ProductService.Model.ProductResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ProductService {
+
+    long addProduct(ProductRequest productRequest);
+
+    ProductResponse getProductById(long productId);
+}
