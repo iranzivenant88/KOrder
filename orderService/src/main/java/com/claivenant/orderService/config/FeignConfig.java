6fe2a8ac6ab354175683c29beac7724812c69509
iranzivenant88 <88venant@gmail.com>
2023-04-08 21:48:44 -0600

    changes

--- orderService/src/main/java/com/claivenant/orderService/config/FeignConfig.java
+++ orderService/src/main/java/com/claivenant/orderService/config/FeignConfig.java
@@ -2,10 +2,12 @@ package com.claivenant.orderService.config;
 
 import com.claivenant.orderService.external.decoder.CustomErrorDecoder;
 import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+    @Bean
     ErrorDecoder errorDecoder(){
         return new CustomErrorDecoder();
     }
