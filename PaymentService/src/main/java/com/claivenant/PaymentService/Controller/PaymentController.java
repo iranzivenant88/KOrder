4c7842741759a2438354f5814c6e7601571d5b12
iranzivenant88 <88venant@gmail.com>
2023-04-08 21:49:01 -0600

    changes-1

---
+++ PaymentService/src/main/java/com/claivenant/PaymentService/Controller/PaymentController.java
@@ -0,0 +1,9 @@
+package com.claivenant.PaymentService.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+}
