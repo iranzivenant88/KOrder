0b8be510b01f4f5feaac0c7669dfe44476d1cbff
iranzivenant88 <88venant@gmail.com>
2023-04-10 23:26:21 -0600

    change2

--- PaymentService/src/main/java/com/claivenant/PaymentService/Service/PaymentService.java
+++ PaymentService/src/main/java/com/claivenant/PaymentService/Service/PaymentService.java
@@ -1,7 +1,9 @@
 package com.claivenant.PaymentService.Service;
 
+import com.claivenant.PaymentService.Model.PaymentRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface PaymentService {
+    long doPayment(PaymentRequest paymentRequest);
 }
