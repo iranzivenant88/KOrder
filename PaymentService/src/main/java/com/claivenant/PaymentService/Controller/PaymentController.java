c7c4898b9ea5be074e73c2c8f1c7a2d2deed0457
iranzivenant88 <88venant@gmail.com>
2023-04-10 23:25:20 -0600

    changes6

--- PaymentService/src/main/java/com/claivenant/PaymentService/Controller/PaymentController.java
+++ PaymentService/src/main/java/com/claivenant/PaymentService/Controller/PaymentController.java
@@ -1,9 +1,25 @@
 package com.claivenant.PaymentService.Controller;
 
+import com.claivenant.PaymentService.Model.PaymentRequest;
+import com.claivenant.PaymentService.Service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/payment")
 public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<Long>doPayment(@RequestBody PaymentRequest paymentRequest){
+        return  new ResponseEntity<>(
+                paymentService.doPayment(paymentRequest), HttpStatus.OK);
+
+    }
 }
