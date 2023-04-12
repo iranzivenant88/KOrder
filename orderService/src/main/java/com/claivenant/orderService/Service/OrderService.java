fb6a06925c2fd17fcac86e814c7b4de4a08f94bd
iranzivenant88 <88venant@gmail.com>
2023-04-12 00:01:03 -0600

    change3

--- orderService/src/main/java/com/claivenant/orderService/Service/OrderService.java
+++ orderService/src/main/java/com/claivenant/orderService/Service/OrderService.java
@@ -1,9 +1,12 @@
 package com.claivenant.orderService.Service;
 
 import com.claivenant.orderService.Model.OrderRequest;
+import com.claivenant.orderService.Model.OrderResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface OrderService {
     Long placeOrder(OrderRequest orderRequest);
+
+    OrderResponse getOrderDetails(long orderId);
 }
