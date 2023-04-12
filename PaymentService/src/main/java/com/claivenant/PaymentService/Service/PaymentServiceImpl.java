9321e2a0b68e41fdcd08572083c97115ce1da19e
iranzivenant88 <88venant@gmail.com>
2023-04-12 00:03:05 -0600

    change9

--- PaymentService/src/main/java/com/claivenant/PaymentService/Service/PaymentServiceImpl.java
+++ PaymentService/src/main/java/com/claivenant/PaymentService/Service/PaymentServiceImpl.java
@@ -1,7 +1,9 @@
 package com.claivenant.PaymentService.Service;
 
 import com.claivenant.PaymentService.Entity.TransactionDetails;
+import com.claivenant.PaymentService.Model.PaymentMode;
 import com.claivenant.PaymentService.Model.PaymentRequest;
+import com.claivenant.PaymentService.Model.PaymentResponse;
 import com.claivenant.PaymentService.Repository.TransactionDetailsRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,24 @@ import java.time.Instant;
 public class PaymentServiceImpl implements PaymentService{
     @Autowired
     private TransactionDetailsRepository transactionDetailsRepository;
+
+    @Override
+    public PaymentResponse getPaymentDetailsByOrderId(String orderId) {
+        log.info("Getting payment for details for orderId: {}");
+        TransactionDetails transactionDetails
+                =transactionDetailsRepository.findByOrderId(Long.valueOf(orderId));
+
+        PaymentResponse paymentResponse
+                =PaymentResponse.builder()
+                .paymentId(transactionDetails.getId())
+                .paymentMode(PaymentMode.valueOf(transactionDetails.getPaymentMode()))
+                .paymentDate(transactionDetails.getPaymentDate())
+                .orderId(transactionDetails.getOrderId())
+                .build();
+
+        return paymentResponse;
+    }
+
     @Override
     public long doPayment(PaymentRequest paymentRequest) {
         log.info("Recording Payment Details :{},paymentRequest");
