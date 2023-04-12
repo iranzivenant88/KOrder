ef7b59d73c6a41968e52872b13171352d66bda99
iranzivenant88 <88venant@gmail.com>
2023-04-12 00:00:05 -0600

    change1

--- orderService/src/main/java/com/claivenant/orderService/Controller/OrderController.java
+++ orderService/src/main/java/com/claivenant/orderService/Controller/OrderController.java
@@ -1,15 +1,13 @@
 package com.claivenant.orderService.Controller;
 
 import com.claivenant.orderService.Model.OrderRequest;
+import com.claivenant.orderService.Model.OrderResponse;
 import com.claivenant.orderService.Service.OrderService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
@@ -22,5 +20,11 @@ public class OrderController {
         Long orderId = orderService.placeOrder(orderRequest);
         log.info("Order ID: {}",orderId);
         return new ResponseEntity<>(orderId, HttpStatus.OK);
+    }
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponse>getOrderDetails(@PathVariable long orderId){
+        OrderResponse orderResponse
+                =orderService.getOrderDetails(orderId);
+        return new ResponseEntity<>(orderResponse,HttpStatus.OK);
     }
 }
