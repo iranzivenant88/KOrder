0b3498ca112052b8768fb2031ec7f5605860c971
iranzivenant88 <88venant@gmail.com>
2023-04-12 00:02:53 -0600

    change9

--- PaymentService/src/main/java/com/claivenant/PaymentService/Service/PaymentService.java
+++ PaymentService/src/main/java/com/claivenant/PaymentService/Service/PaymentService.java
@@ -1,9 +1,12 @@
 package com.claivenant.PaymentService.Service;
 
 import com.claivenant.PaymentService.Model.PaymentRequest;
+import com.claivenant.PaymentService.Model.PaymentResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface PaymentService {
+    PaymentResponse getPaymentDetailsByOrderId(String orderId);
+
     long doPayment(PaymentRequest paymentRequest);
 }
