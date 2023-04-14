5b41430a3352e999041afdb74449a6350b07afaf
iranzivenant88 <88venant@gmail.com>
2023-04-13 23:05:28 -0600

    change8

--- ProductService/src/main/java/com/claivenant/ProductService/ProductServiceApplication.java
+++ ProductService/src/main/java/com/claivenant/ProductService/ProductServiceApplication.java
@@ -2,6 +2,7 @@ package com.claivenant.ProductService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 
 @SpringBootApplication
 
