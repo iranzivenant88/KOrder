99eea5b7afa61944fabbafe6459be5c1b17aca5e
iranzivenant88 <88venant@gmail.com>
2023-04-15 00:08:53 -0600

    change1

--- orderService/src/main/java/com/claivenant/orderService/external/client/PaymentService.java
+++ orderService/src/main/java/com/claivenant/orderService/external/client/PaymentService.java
@@ -8,13 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@CircuitBreaker(name = "external",fallbackMethod ="fallback")
+@CircuitBreaker(name = "external",fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
     @PostMapping
    ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
 
     default void fallback(Exception e){
-        throw new CustomException("Payment Service is not available","UNAVAILABLE",500);
+        throw new CustomException("Payment Service is not available",
+                "UNAVAILABLE", 500);
     }
 }
