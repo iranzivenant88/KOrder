b55cefb95c56ebda694e0d19d76e3e2fb2137268
iranzivenant88 <88venant@gmail.com>
2023-04-15 23:32:10 -0600

    change1

---
+++ CloudGateWay /src/main/java/com/claivenant/CloudGateWay/security/OktaOAuth2WebSecurity.java
@@ -0,0 +1,30 @@
+package com.claivenant.CloudGateWay.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@EnableWebFluxSecurity
+public class OktaOAuth2WebSecurity {
+
+    @Bean
+    public SecurityWebFilterChain  securityWebFilterChain(ServerHttpSecurity http){
+        http
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2Login()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+
+
+    }
+
+}
